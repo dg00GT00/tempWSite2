4fb7f5f76061508a136642c3fcdbe225f653cbe8
Gledson Duarte <duarte.gledson@gmail.com>
2020-02-27 09:01:48 -0300

    Made the photo frame work properly

--- src/app/main-header/main-header.component.ts
+++ src/app/main-header/main-header.component.ts
@@ -39,6 +39,28 @@ export class MainHeaderComponent implements OnInit, AfterViewInit {
     ) {
     }
 
+    private dynPhotoFrame(viewRef: EmbeddedViewRef<null>) {
+        // The gotten angle should be divided for a tax to keep the right inclination
+        this.polygonAngleService.getAngleById('left-clip').subscribe((degAngle: number) => {
+            // on the component that bas being observable
+            // Got the first and only node on the rootNodes array
+            const htmlElement: HTMLElement[] = viewRef.rootNodes;
+            const dynValue = degAngle / this.correctionTax;
+            const value = `rotate(${-dynValue}deg) scale(1, 1.009) skewY(${dynValue}deg`;
+            if (RegExp(/[01]/).test(htmlElement[0].className)) {
+                this.renderer.setStyle(htmlElement[0], 'transform', value);
+                htmlElement[0].childNodes.forEach(childNode => {
+                    // Is needed skip a comment element that comes from ng-container
+                    if (!RegExp(/comment/).test(childNode.nodeName)) {
+                        this.renderer.setStyle(childNode, 'transform', `skewY(${-dynValue}deg)`);
+                    }
+                });
+            } else {
+                this.renderer.setStyle(htmlElement[0], 'transform', `rotate(${-dynValue}deg)`);
+            }
+        });
+    }
+
     ngOnInit(): void {
         this.polygonConfigLeft = {
             Right: {degAngle: 18.4, clipCorner: 'Up'},
@@ -50,6 +72,7 @@ export class MainHeaderComponent implements OnInit, AfterViewInit {
 
     ngAfterViewInit(): void {
         this.templatesRef.forEach((templateRef, index) => {
+            // Is needed skip a comment element that comes from ng-container
             if (index >= 1) {
                 this.viewRefArray.push(this.containerRef.createEmbeddedView(templateRef));
             }
@@ -58,21 +81,7 @@ export class MainHeaderComponent implements OnInit, AfterViewInit {
         const mainPhotoIndex = this.containerRef.indexOf(lastViewRef);
         this.containerRef.detach(mainPhotoIndex);
         this.containerRef.insert(lastViewRef, 1);
-        this.viewRefArray.forEach(viewRef => {
-            // The gotten angle should be divided for a tax to keep the right inclination
-            this.polygonAngleService.getAngleById('left-clip').subscribe((degAngle: number) => {
-                // on the component that bas being observable
-                // Got the first and only node on the rootNodes array
-                const htmlElement: HTMLElement[] = viewRef.rootNodes;
-                const dynValue = degAngle / this.correctionTax;
-                const value = `rotate(${-dynValue}deg) scale(1, 1.009) skewY(${dynValue}deg`;
-                if (RegExp(/[01]/).test(htmlElement[0].className)) {
-                    this.renderer.setStyle(htmlElement[0], 'transform', value);
-                } else {
-                    this.renderer.setStyle(htmlElement[0], 'transform', `rotate(${-dynValue}deg)`);
-                }
-            });
-        });
+        this.viewRefArray.forEach(viewRef => this.dynPhotoFrame(viewRef));
         this.changeDetectorRef.detectChanges();
     }
 }
